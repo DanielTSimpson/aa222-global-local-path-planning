93d1c9e87475b310f7da52e871819bca1cc439fa
Will <wkcovington@gmail.com>
2026-05-28 08:16:24 -0700

    made changes to monte carlo sim

--- config.py
+++ config.py
@@ -53,8 +53,8 @@ LOCAL_PLANNER_TYPE = "cross_entropy" # options are simulated_annealing, cross_en
 
 # TODO LATER: rather than have to tune each weight individually, it could be good to just have different run types
 # for instance, we could have a "science-focused" run where science is weighted above a nominal value, or a "battery-focused" run and so on
-SIMANNEAL_WEIGHTS = {"science": 10.0, "explore": 2.0, "battery": 1.0, "obstacle": 100.0, "path": 5.0, "recovery": 10.0}
-CEM_WEIGHTS = {"science": 10.0, "explore": 2.0, "battery": 1.0, "obstacle": 100.0, "path": 5.0, "recovery": 10.0}
+SIMANNEAL_WEIGHTS = {"science": 10.0, "explore": 2.0, "battery": 1.0, "obstacle": 100.0, "path": 5.0, "recovery": 10.0, "revisit": 20.0}
+CEM_WEIGHTS = {"science": 10.0, "explore": 2.0, "battery": 1.0, "obstacle": 100.0, "path": 5.0, "recovery": 10.0, "revisit": 20.0}
 
 # ===== Logging/Terminal Output =====
 VERBOSE_LOGGING = False
